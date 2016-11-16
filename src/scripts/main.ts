50926e3fec73068ab8a454b7e0991bef90542be7
Fernando Lira <Fernando Lira>
2016-11-15 23:21:35 -0200

    Improved typescript compilation

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,20 +1,10 @@
-/// <reference path="definitely-typed/jquery.d.ts" />
-'use strict';
 import {ScrollAddress} from 'scripts/components/typescripts/scroll-address.js';
 import {ScrollNav} from 'scripts/components/scroll-nav.js';
 //import {SelectiveFix} from 'scripts/components/typescripts/selective-fix.js';
 import {ToggleItem} from 'scripts/components/toggle-item.js';
 import {LoadWorkTiles} from 'scripts/components/typescripts/load-work-tiles.js';
 
-interface Components {
-  lw: LoadWorkTiles;
-  sa: ScrollAddress;
-  sn: ScrollNav;
-  //sf: SelectiveFix;
-  ti: ToggleItem;
-}
-
-(function(c: Components): void {
+(function(c: Object): void {
   $.each(c, (i, val): void => {c[i].init();});
 })({
   lw: new LoadWorkTiles(),
