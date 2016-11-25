9149831f7b23a35456c9430779a42245ee1b008e
Fernando Lira <fernando@lira.etc.br>
2016-11-25 00:56:50 -0200

    Json update

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,11 +3,13 @@
 import { ScrollNav } from './components/typescripts/scroll-nav';
 import { ToggleItem } from './components/typescripts/toggle-item';
 import { ScrollAddress } from './components/typescripts/scroll-address';
-import { CONST } from './components/typescripts/constants';
+import { ShowProject } from './components/typescripts/show-project';
 import { LoadWorkTiles } from './components/typescripts/load-work-tiles';
+import { CONST } from './components/typescripts/constants';
 
 (function(components: Object){
   Object.freeze(CONST);
+  components['showProject'].showProject();
   for (const component in components) {
     if ('init' in components[component]) {
       (<Component>components[component]).init();
@@ -17,5 +19,6 @@ import { LoadWorkTiles } from './components/typescripts/load-work-tiles';
   loadWorkTiles: new LoadWorkTiles,
   scrollAddress: new ScrollAddress,
   scrollNav: new ScrollNav, // Animate ID anchors scroll
+  showProject: new ShowProject,
   toggleItem: new ToggleItem
 }));
