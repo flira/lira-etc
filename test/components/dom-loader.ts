a9e6ae736019427279377f31240237070a55e2cc
Fernando Lira <fernando@lira.etc.br>
2017-01-09 17:31:53 -0200

    Fixed some tests issues

--- test/components/dom-loader.ts
+++ test/components/dom-loader.ts
@@ -1,24 +1,31 @@
-"use strict";
-const
-  fs = require('fs'),
-  jsdom = require('jsdom');
+// "use strict";
+// const
 
-export const DOMinate: (method: () => void) => void =
-  function (method: () => void): void {
-    const
-      html: string = fs.readFileSync('src/index.html', 'utf-8'),
-      jQuery: string = fs.readFileSync('node_modules/jquery/dist/jquery.js', 'utf-8'),
-      done: (error: any, window: any) => void =
-        function (error: Error, window: Window): void {
-          if (!error) {
-            global['document'] = window.document;
-            global['$'] = window.hasOwnProperty('jQuery') ? window['jQuery'] : undefined;
-            method();
-          } else {
-            console.warn(error);
-          }
-          return void 0;
-        };
-    jsdom.env({html: html, src: [jQuery], done: done});
-    return void 0;
-  };
+
+// export class LoadDom {
+//   private HTML: string = fs.readFileSync('src/index.html', 'utf-8');
+//   private JQUERY: string = fs.readFileSync('node_modules/jquery/dist/jquery.js', 'utf-8');
+//   protected assets;
+
+//   public test() {}
+
+//   public start(): void {
+//     jsdom.env({html: this.HTML, src: [this.JQUERY], done: this.done});
+//     console.log(typeof this.test + '1');
+//   }
+
+//   private done(error: Error, window: Window): void {
+//     if (!error) {
+//       this.assets = new Map([
+//         ['window', window],
+//         ['document', window.document]
+//       ]);
+//       for (var i in this) console.log(i);
+//       this.test();
+//     } else {
+//       console.warn(error);
+//     }
+
+//     return void 0;
+//   }
+// }
--- test/toggle-item-test.ts
+++ test/toggle-item-test.ts
@@ -1,11 +1,33 @@
-import { ToggleItem } from './units/ToggleItem';
-import { DOMinate } from './components/dom-loader';
-const expect = require ('chai').expect;
+//Imports
+const
+  fs = require('fs'),
+  jsdom = require('jsdom'),
+  assert = require ('chai').assert;
 
-DOMinate(
-  function ():void {
-    const toggleItem = new ToggleItem;
-    console.log(toggleItem);
-    return void 0;
+const
+  html: string = fs.readFileSync('src/index.html', 'utf-8'),
+  jquery: string = fs.readFileSync('node_modules/jquery/dist/jquery.js', 'utf-8');
+
+function done (e: Error, w: Window) {
+  if (!e) {
+    global['window'] = w;
+    global['document'] = w.document;
+    global['$'] = 'jQuery' in w ? w['jQuery'] : undefined;
+    run();
+  } else {
+    console.error(e);
   }
-);
+}
+
+jsdom.env({html: html, src: [jquery], done: done});
+
+describe(`ToggleItem Class`, () => {
+  it('Should have no active item at start', () => {
+    assert.equal($('.active[data-toggle-for]').length, 0);
+  });
+  it('Should have one active item after click and should be the clicked item', () => {
+    let item: HTMLElement = <HTMLElement>document.querySelector('[data-toggle-for]');
+    item.click();
+    assert.equal($('[data-toggle-for]').eq(0).hasClass('active'), true);
+  });
+});
